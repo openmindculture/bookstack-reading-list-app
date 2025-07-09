2759c6c7e32e6d499f6362b8ba492513160bb107
Ingo Steinke <ingosteinke@gmx.de>
2025-07-09 12:11:45 +0200

    fix eslint astro typescript configuration

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+import astroParser from "astro-eslint-parser";
+
+export default tseslint.config(
+	...eslintPluginAstro.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ["**/*.astro"],
+		languageOptions: {
+			parser: astroParser,
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+				extraFileExtensions: [".astro"]
+			}
+		},
+		rules: {}
+	},
+	{
+		files: ["**/*.{js,ts,jsx,tsx}"],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {}
+		},
+		rules: {}
+	}
+);
