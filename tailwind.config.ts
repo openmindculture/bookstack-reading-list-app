88d98e09d35e63375d39a9e5c8e7dcce0b8dfb4a
Ingo Steinke <ingosteinke@gmx.de>
2025-07-09 17:30:26 +0200

    global tailwind typography (WIP)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,ts,tsx}',
+		'./src/styles/**/*.css',
 		'./public/**/*.html'
 	],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				'atkinson': ['Atkinson Hyperlegible', 
+					...defaultTheme.fontFamily.sans],
+				'dx-kord': ['Dx Kord', ...defaultTheme.fontFamily.sans],
+			},
+		}
 	},
 	plugins: []
 };
