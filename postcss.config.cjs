077178a46d5f752cba37d0ec34ef72f145bd278a
Ingo Steinke <ingosteinke@gmx.de>
2025-07-09 21:30:53 +0200

    replace deprecated postcss integration

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("postcss-load-config").Config} */
 module.exports = {
 	plugins: {
-		tailwindcss: {}, // must come first
+		'@tailwindcss/postcss': {}, // must come first
 		autoprefixer: {}
 	}
 };
