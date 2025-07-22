386a1dec67490604360d702bfd455840648725b2
Ingo Steinke <ingosteinke@gmx.de>
2025-07-22 15:24:38 +0200

    fix compatibility (WIP)

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("postcss-load-config").Config} */
-module.exports = {
-	plugins: {
-		'@tailwindcss/postcss': {}, // must come first
-		autoprefixer: {}
-	}
-};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,19 +3,19 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
 	content: [
-		"./src/**/*.{astro,html,js,jsx,ts,tsx}",
-		"./src/**/*.css",
-		"./public/**/*.html"
+		'./src/**/*.{astro,html,jsx,tsx}',
+		'./src/**/*.css',
+		'./public/**/*.html'
 	],
 	theme: {
 		extend: {
 			fontFamily: {
-				"atkinson": ["Atkinson Hyperlegible",
+				'atkinson': ['Atkinson Hyperlegible',
 					...defaultTheme.fontFamily.sans],
-				"dx-kord": ["Dx Kord", ...defaultTheme.fontFamily.sans]
+				'dx-kord': ['Dx Kord', ...defaultTheme.fontFamily.sans]
 			},
 			fontSize: { /* generated classes automatically prefix with "text-" */
-				"clamp-10ch": "clamp(5rem, 10ch, 24rem)"
+				'clamp-10ch': 'clamp(5rem, 10ch, 24rem)'
 			},
 			colors: { // ADD THIS BLOCK generating text-test-magenta class
 				'test-magenta': '#FF00FF',
