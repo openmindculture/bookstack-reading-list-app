ddd05b59e44921c3e2594ff2f8c4aefa11ec8d05
Ingo Steinke <ingosteinke@gmx.de>
2025-08-07 21:01:04 +0200

    global storybook style

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react-vite'
+import '../src/styles/global.css';
+console.log('storybook preview ts executed');
 
 const preview: Preview = {
   parameters: {
