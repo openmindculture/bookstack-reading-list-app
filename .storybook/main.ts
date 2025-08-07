37ddca5b24dd50a244996f17c859828cd88d32f7
Ingo Steinke <ingosteinke@gmx.de>
2025-08-07 21:00:14 +0200

    remove obsolete chromatic addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,14 +6,13 @@ const config: StorybookConfig = {
 		'../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
 	],
 	'addons': [
-        '@chromatic-com/storybook',
         '@storybook/addon-docs',
         '@storybook/addon-a11y',
-        '@storybook/addon-vitest'
+        '@storybook/addon-vitest',
     ],
 	'framework': {
 		'name': '@storybook/react-vite',
 		'options': {}
-	}
+	},
 };
 export default config;
