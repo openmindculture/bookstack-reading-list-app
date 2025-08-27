7849d79b01d77d4d4a88a19c20e26bbb9d6c80b9
Ingo Steinke <ingosteinke@gmx.de>
2025-08-27 22:23:54 +0200

    prepare sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 // import type { PluginOption } from 'vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  // You do NOT need the vite.plugins entry for Tailwind
-  // when using the @astrojs/tailwind integration.
+  integrations: [react(), tailwind(), sitemap()],
   vite: {},
   site: 'https://dev-ux-lesezeichen.de',
   base: '/',
